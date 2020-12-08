51d22b839aadb278ed5ee9715e170527de7a0314
zixiangwei <mir1102@163.com>
2020-12-08 18:51:41 +0800

    classifier added

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 import SocketServer
 import socket
 import pickle
+import numpy as np
+from classifier import Classifier
+from PIL import Image
+import cv2
 
 encoding = 'utf-8'
 BUFSIZE = 1024
@@ -8,14 +12,16 @@ BUFSIZE = 1024
 class MyServer(SocketServer.StreamRequestHandler):
     def handle(self):
         while True:
-            data = self.request.recv(BUFSIZE)
-            if data:
-                img = pickle.loads(data)
-                
-                # 
-                # res = classifier.predict(img)
+            length = self.request.recv(16)
+            if length:
+                str_data = self.request.recv(int(length))
 
-                res = "hello, client"
+                data = np.frombuffer(str_data, np.uint8)
+                img = cv2.imdecode(data, cv2.IMREAD_COLOR)
+
+                clf = Classifier()
+                # img = Image.open("data/dog.jpg")
+                res = clf.predict(img)
                 print(res)
                 self.request.send(pickle.dumps(res))
             else:
