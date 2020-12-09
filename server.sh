03e5931bb2b977522dc8d97789589521e962dfd1
Zixiang Wei <56742281+zxwei1102@users.noreply.github.com>
2020-12-09 06:23:42 -0500

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install python3-pip
+sudo pip3 install numpy
+sudo pip3 install pillow
+
+sudo pip3 install torch===1.4.0 torchvision===0.5.0 -f https://download.pytorch.org/whl/torch_stable.html --no-cache-dir 
+
+wget https://raw.githubusercontent.com/lux1997/CS655_grp/main/server.py
+wget https://raw.githubusercontent.com/lux1997/CS655_grp/main/classifier.py
