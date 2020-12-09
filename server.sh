5b138f09b6b8113571ae37a10a28456ff4834128
Zixiang Wei <56742281+zxwei1102@users.noreply.github.com>
2020-12-09 11:39:53 -0500

    Update server.sh

--- server.sh
+++ server.sh
@@ -7,3 +7,9 @@ sudo pip3 install --no-cache-dir torch
 
 wget https://raw.githubusercontent.com/lux1997/CS655_grp/main/server.py
 wget https://raw.githubusercontent.com/lux1997/CS655_grp/main/classifier.py
+
+mkdir data
+cd data
+wget https://raw.githubusercontent.com/lux1997/CS655_grp/main/data/imagenet_classes.txt
+wget https://raw.githubusercontent.com/lux1997/CS655_grp/main/data/imagenet_synsets.txt
+cd ..
