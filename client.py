96ced0965afda306c78931e223f46f7fc078066e
lux1997 <60154209+lux1997@users.noreply.github.com>
2020-12-09 13:15:23 -0500

    Update client.py

--- client.py
+++ client.py
@@ -43,12 +43,19 @@ while(num<10):
 ##################return the html page with rcv result################
 
 ############## connnection part ends
-print('Content-type:text/html\n\n')
-print('<html>')
-print('  <head>')
-print('    <h1>Recognition complete!</h1>') 
-print('  </head>')
-print('</html>')
+print ("""\
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <title>CS655 geni project</title>
+    </head>
+    <body>
+    <h1>Recognition complete!</h1>
+    <p>The picture that you upload is:</p>
+    </a>
+    </body>
+    </html>
+    """)
 else:
     print ("""\
     <html>
